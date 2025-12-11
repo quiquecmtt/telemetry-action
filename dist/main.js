b1e9ed58a12cd9e67eea1e4a225760d100697db7
Enrique Cametti <quiquecametti97@gmail.com>
2025-12-11 19:59:01 -0300

    Fix: remove sourcemaps from build

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,4 @@
-require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
+/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
 /***/ 4914:
@@ -27729,4 +27729,3 @@ module.exports = parseParams
 /******/ 	
 /******/ })()
 ;
-//# sourceMappingURL=index.js.map
